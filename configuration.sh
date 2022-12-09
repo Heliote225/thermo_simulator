6ff84a2e565d63529b7b36bec2536ae2fcc66094
Heliote225 <116874669+Heliote225@users.noreply.github.com>
2022-12-09 19:33:04 +0100

    update 1

--- configuration.sh
+++ configuration.sh
@@ -1,13 +1,15 @@
 #!/bin/bash  
 echo "Beginning of configuration"
-echo "Moving of the python files to /home/pi/Desktop/" 
+echo "Moving of the python files to /home/pi/Desktop/"
+sudo chmod 777 *
+sudo chmod 777 ./html/*
+sudo chmod 777 /var/www/html
 mv *.py /home/pi/Desktop/ 
 mv *.png /home/pi/Desktop/ 
 mv *.jpg /home/pi/Desktop/ 
 mv *.ttf /home/pi/Desktop/
 echo "Moving of the html content to /var/www/html/" 
 sudo mv html/* /var/www/html/
-sudo chmod 777 /var/www/html/*
 echo "configuration done succesfully!"  
 #SOMEVAR='text stuff'  
 #echo "$SOMEVAR"

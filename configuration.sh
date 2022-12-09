11143a0c0f057633a56c5a96a15ac6617b45594b
Heliote225 <116874669+Heliote225@users.noreply.github.com>
2022-12-09 19:42:13 +0100

    update 2

--- configuration.sh
+++ configuration.sh
@@ -9,7 +9,7 @@ mv *.png /home/pi/Desktop/
 mv *.jpg /home/pi/Desktop/ 
 mv *.ttf /home/pi/Desktop/
 echo "Moving of the html content to /var/www/html/" 
-sudo mv html/* /var/www/html/
+sudo mv html/ /var/www/
 echo "configuration done succesfully!"  
 #SOMEVAR='text stuff'  
 #echo "$SOMEVAR"

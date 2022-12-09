7476d8d8e328e5cbfdba9016fa434dc0e578b49c
Heliote225 <116874669+Heliote225@users.noreply.github.com>
2022-12-09 22:39:36 +0100

    update 10

--- configuration.sh
+++ configuration.sh
@@ -9,6 +9,11 @@ sudo chown -R pi:www-data /var/www/html/
 sudo apt install php php-mbstring
 sudo apt install mariadb-server php-mysql
 sudo chmod -R 770 /var/www/html/
+echo "Done" 
+echo "Configuration of the database measure_db"
+sudo chmod +x data.sql
+sudo mysql < data.sql
+echo "Done"
 echo "Moving of the python files to /home/pi/Desktop/"
 sudo chmod 777 *
 sudo chmod 777 ./html/*
@@ -17,9 +22,9 @@ mv *.py /home/pi/Desktop/
 mv *.png /home/pi/Desktop/ 
 mv *.jpg /home/pi/Desktop/ 
 mv *.ttf /home/pi/Desktop/
+echo "Done" 
 echo "Moving of html/ to /var/www/" 
 sudo rm -rf /var/www/html
 sudo mv html/ /var/www/
+echo "Done" 
 echo "configuration done succesfully!"  
-#SOMEVAR='text stuff'  
-#echo "$SOMEVAR"
